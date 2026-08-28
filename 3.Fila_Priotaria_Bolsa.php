b993a172e5b2904a0db7f290659e04b381c0702b
Katllyn Rezende <Katllynrezende@gmail.com>
2026-08-28 20:39:13 -0300

    Exertcício 3

---
+++ 3.Fila_Priotaria_Bolsa.php
@@ -0,0 +1,12 @@
+<?php
+
+$Idade = 11;
+$TempoEspera = 30;
+
+If ($Idade >= 60 && $TempoEspera >= 30) {
+    echo "O motorista tem prioridade na fila de embarque.";
+} else {
+    echo "O motorista não tem prioridade na fila de embarque.";
+}
+
+?>
