1f3036e6264ba47d4dd9f211fab7d769ef91a9fe
Katllyn Rezende <Katllynrezende@gmail.com>
2026-08-28 21:19:36 -0300

    Exercício 5

---
+++ 5.Classificador_DDD.php
@@ -0,0 +1,15 @@
+<?php
+
+$DDD = 11;
+
+Match ($DDD) {
+    11 => print("São Paulo"),
+    21 => print("Rio de Janeiro"),
+    32 => print("Juiz de Fora"),
+    19 => print("Campinas"),
+    27 => print("Vitória"),
+    31 => print("Belo Horizonte"),
+    default => print("DDD não cadastrado")
+};
+
+?>
