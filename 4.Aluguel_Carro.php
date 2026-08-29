e2eb1c23b638fbb8087b51cd495e5b5ab98fae8d
Katllyn Rezende <Katllynrezende@gmail.com>
2026-08-28 21:17:23 -0300

    Exercício 4

---
+++ 4.Aluguel_Carro.php
@@ -0,0 +1,11 @@
+<?php
+
+$Idade = 25;
+$Renda = 2500;
+
+If ($Idade >= 25 || $Renda > 7000){
+    echo "Você pode alugar na categoria luxo";
+} else {
+    echo "Você não pode alugar na categoria luxo";
+}
+?>
